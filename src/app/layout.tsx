5aecc547c658cb88f0aececb84e46150401faa04
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-04-13 10:12:42 +0530

    setup ui library

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Grow Together - Your Gateway to Top Job Opportunities",
+  description:
+    "Discover and apply to jobs from multiple platforms like LinkedIn, Indeed, and Naukri, all in one place. Stay ahead in your career with our job aggregator platform.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${monaSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
